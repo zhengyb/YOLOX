f9004777e794db1fbf1b1902204fc27bc3c7e7aa
zhengyb <zhengyb@inhand.com.cn>
2025-03-13 16:50:33 -0400

    1. change classes number for COCO example dataset; 2. add exp file for yolox_s_bdd

--- exps/default/yolox_s_bdd.py
+++ exps/default/yolox_s_bdd.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 # Copyright (c) Megvii, Inc. and its affiliates.
-
 import os
 
 from yolox.exp import Exp as MyExp
@@ -10,7 +9,17 @@ from yolox.exp import Exp as MyExp
 class Exp(MyExp):
     def __init__(self):
         super(Exp, self).__init__()
+        self.num_classes = 10
         self.depth = 0.33
         self.width = 0.50
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
-        self.num_classes = 71 # 71个类别 for coco128
+
+        # Define yourself dataset path
+        self.data_dir = "datasets/BDD"
+        self.train_ann = "instances_train.json"
+        self.val_ann = "instances_val.json"
+        self.test_ann = "instances_test.json"
+
+        self.max_epoch = 300
+        self.data_num_workers = 4
+        self.eval_interval = 1
